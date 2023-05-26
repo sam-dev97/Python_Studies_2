2ffb90c4268224f09e09cb68b1ad1d067d3baa50
Samuel Santos <vivatecnosams@gmail.com>
2023-05-26 11:44:40 -0300

    Alterado mecanismo de exibição Alterado o design dos botões e da interface Adicionada função: Buscar por nome Adicionado campo: Data de registro Alterada disposição dos campos na interface

--- Registro__.py
+++ Registro__.py
@@ -1,153 +1,70 @@
 import csv
 import tkinter as tk
-from tkinter import messagebox
-
-# Função para salvar os registros efetuados
-def salvar_registro(nome, idade, email):
-    # Abrindo o arquivo CSV que contem as informações de registro
-    with open('registros1.csv', mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([nome, idade, email])
-    # Exibe uma mensagem quando algum usuário é registrado(apenas para confirmar mesmo)
-    messagebox.showinfo('Registro', 'Usuário registrado com sucesso!')
-
-# Exibe os registros existentes
-def exibir_registros():
-    # Abrindo o arquivo CSV que contem as informações de registro
-    with open('registros1.csv', mode='r') as file:
-        reader = csv.reader(file)
-        registros = list(reader)
-    # Condicioinal + list comprehensionzinha básica para checar/imprimir os registros existentes
-    if registros:
-        mensagem = '\n'.join([f'Nome: {registro[0]} |||| Idade: {registro[1]} |||| Email: {registro[2]}' for registro in registros])
-    else:
-        # Se não tiver nada registrado, o que estiver abaixo será exibido posterior mente como msg de erro
-        mensagem = 'Nenhum registro encontrado.'
-    # Chamada da msg de erro apenas com uma "infobox"
-    #messagebox.showinfo('Registros', mensagem)
-    # Criando uma janela para exibir as informações, anters com messagebox o tamanho deixava feio
-    janela_registros = tk.Toplevel()
-    janela_registros.title("Registros")
-    janela_registros.geometry("500x150")
-    # Não permitir alteração no tamanho da janela com as informações
-    janela_registros.resizable(width= False, height=False)
-    # Pequeno título
-    label_registros = tk.Label(janela_registros, text= "Registros:")
-    label_registros.pack()
-    # Informações de registro
-    label_registros_2 = tk.Label(janela_registros, text= mensagem, justify='left')
-    label_registros_2.pack()
-    # Botão para fechar a janela de exibição de registros
-    botao_fechar_registros = tk.Button(janela_registros, text="Fechar", command=janela_registros.destroy)
-    botao_fechar_registros.pack()
-
-# Função para inserir o registro
-def cadastrar():
-    # Aqui coletamos o conteúdo das text_box para adicionarmos no arquivo de registro
-    nome = entry_nome.get()
-    idade = entry_idade.get()
-    email = entry_email.get()
-    # Checando se existe algo escrito nos campos nome e idade, o registro deve ser completo
-    if nome and idade and email:
-        salvar_registro(nome, idade, email)
-        entry_nome.delete(0, tk.END)
-        entry_idade.delete(0, tk.END)
-        entry_email.delete(0, tk.END)
-    else:
-        # showwarning exibe uma mensagem de "aviso" alertando que tem falta alguma informação
-        messagebox.showwarning('Campos vazios', 'Por favor, preencha todos os campos.')
-        
-# Aqui deu um trabalho da peste, mas no fim brilhei
-# Função para apagar algum registro
-def apagar_registro():
-    # Abrindo o arquivo CSV que contem as informações de registro
-    with open('registros1.csv', mode='r') as file:
-        reader = csv.reader(file)
-        registros = list(reader)
-    # Tradução literal: Se existirem registros...
-    if registros:
-        # Exibindo uma caixa de diálogo com a lista de registros para o usuário escolher qual excluir
-        # Odeio list comprehension, que fique claro
-        opcoes = [f'Nome: {registro[0]}, Idade: {registro[1]}, E-mail: {registro[2]}' for registro in registros]
-        escolha = tk.StringVar()  # Armazena temporatiamente a escolha do usuário
-        escolha.set(opcoes[0])  # Definindo o valor padrão da escolha como o primeiro registro(**spoiler: não funcionou) 
-
-        # Criando uma janela para exibir a lista de registros
-        janela = tk.Toplevel()
-        janela.title('Excluir Registro')
-
-        # Criando uma list box com as opções de registros
-        lista_registros = tk.Listbox(janela, height=10)
-        # Insere ao final da lista as opções de registro
-        for opcao in opcoes:
-            lista_registros.insert(tk.END, opcao)
-        lista_registros.pack()
-
-        def confirmar_exclusao():
-            # Obtendo o índice do registro selecionado
-            indice = lista_registros.curselection()[0]
-            # Obtendo o registro selecionado
-            registro_selecionado = registros[indice]
-            # Removendo o registro da lista
-            registros.pop(indice)
-
-            # Sobrescrevendo o arquivo CSV com os registros atualizados
-            with open('registros1.csv', mode='w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerows(registros)
-
-            janela.destroy()  # Fecha a janela de seleção de registros
-            messagebox.showinfo('Registro Removido', f'O registro {registro_selecionado} foi removido com sucesso!')
-
-        # Botão para confirmar a exclusão
-        botao_confirmar = tk.Button(janela, text='Confirmar', command=confirmar_exclusao)
-        botao_confirmar.pack()
-        # Essa bobagem mantem a janela aberta
-        janela.mainloop()
-
-    else:
-        messagebox.showinfo('Nenhum Registro', 'Nenhum registro encontrado para exclusão.')
+import funcoes_registro as ff
+from funcoes_registro import *
+from tkinter import messagebox, ttk
 
 # Criação da interface gráfica da janela principal
 janela_principal = tk.Tk()
 janela_principal.title('Sistema de Registro')
 # Setando o tamanho da janela principal
-janela_principal.geometry('350x150')
+janela_principal.geometry('350x200')
 # Setando uma cor para janela principal
 janela_principal.configure(background='#98E6C5')
+janela_principal.resizable(width=False, height=False)
+
+# Cores
+cor_label = "#98E6C5"
 
 # Label e Entry(text_field) para o nome
 label_nome = tk.Label(janela_principal, text='Nome:')
 label_nome.place(x=15, y=10)
-label_nome.config(bg='#98E6C5')
-entry_nome = tk.Entry(janela_principal, width=30, justify='center')
+label_nome.config(bg=cor_label)
+entry_nome = tk.Entry(janela_principal, width=44, justify='center')
 entry_nome.place(x=65, y=10)
-
-
-# # Label e Entry(text_field) para a idade
-label_idade = tk.Label(janela_principal, text='Idade:')
-label_idade.place(x=260, y=10)
-label_idade.config(bg='#98E6C5')
-entry_idade = tk.Entry(janela_principal, width=5, justify='center')
-entry_idade.place(x=300, y=10)
 
 # # Label e Entry(text_field) para e-mail
 label_email = tk.Label(janela_principal, text='E-mail:')
 label_email.place(x=15, y=45)
-label_email.config(bg='#98E6C5')
+label_email.config(bg=cor_label)
 entry_email = tk.Entry(janela_principal, width=44,  justify='center')
 entry_email.place(x=65, y=45)
 
+# # Label e Entry(text_field) para a idade
+label_idade = tk.Label(janela_principal, text='Idade:')
+label_idade.place(x=15, y=75)
+label_idade.config(bg=cor_label)
+entry_idade = tk.Entry(janela_principal, width=5, justify='center')
+entry_idade.place(x=65, y=75)
+
+# # Label e Entry(text_field) para a data
+label_data = tk.Label(janela_principal, text='Data de cadastro:')
+label_data.place(x=125, y=75)
+label_data.config(bg=cor_label)
+entry_data = tk.Entry(janela_principal, width=16, justify='center')
+entry_data.place(x=230, y=75)
+
 # Botão par a ação cadastrar
-botao_cadastrar = tk.Button(janela_principal, text='Cadastrar', command=cadastrar)
-botao_cadastrar.place(x=45, y=80)
+botao_cadastrar = tk.Button(janela_principal, text='Cadastrar', command=lambda: ff.cadastrar(entry_data, entry_nome, entry_idade, entry_email))
+botao_cadastrar.place(x=45, y=100)
 
 # Botão para ação exibir_registros
-botao_registros = tk.Button(janela_principal, text='Registros', command=exibir_registros)
-botao_registros.place(x=130, y=80)
+botao_registros = tk.Button(janela_principal, text='Registros', command=ff.exibir_registros)
+botao_registros.place(x=130, y=100)
 
 # Botão para ação apagar_registro
-botao_apagar_registros = tk.Button(janela_principal, text='Apagar Registro', command=apagar_registro)
-botao_apagar_registros.place(x=215, y=80)
+botao_apagar_registros = tk.Button(janela_principal, text='Apagar Registro', command=ff.apagar_registro)
+botao_apagar_registros.place(x=215, y=100)
+
+# Barra para pesquisa por nome
+label_pesquisa_nome = tk.Label(janela_principal, text="Pesquisa por nome")
+label_pesquisa_nome.place(x=45, y=140)
+label_pesquisa_nome.config(bg=cor_label)
+
+entry_pesquisa_nome = tk.Entry(janela_principal, width=25)
+entry_pesquisa_nome.place(x=160, y=140)
+
+botao_pesquisa_nome = tk.Button(janela_principal, text='Pesquisar', command=lambda: ff.pesquisa_nome(entry_pesquisa_nome))
+botao_pesquisa_nome.place(x=150, y=165)
 
 janela_principal.mainloop()
---
+++ funcoes_registro.py
@@ -0,0 +1,166 @@
+import csv
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+def salvar_registro(data, nome, idade, email):
+    # Abrindo o arquivo CSV que contem as informações de registro
+    with open('registros1.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([data, nome, idade, email])
+
+    messagebox.showinfo('Registro', 'Usuário registrado com sucesso!')
+    
+    
+def exibir_registros():
+    # Abrindo o arquivo CSV que contem as informações de registro
+    with open('registros1.csv', mode='r') as file:
+        reader = csv.reader(file)
+        registros = list(reader)
+        
+    if registros:
+        # Criando uma janela para exibir a lista de registros
+        janela_rg = tk.Toplevel()
+        janela_rg.title('Registros')
+        janela_rg.geometry("600x250")
+            
+        tvrg = ttk.Treeview(janela_rg, columns=('data','nome', 'idade', 'email',), show='headings')
+         
+        tvrg.column('data', minwidth=0, width=90)   
+        tvrg.column('nome', minwidth=0, width=200)
+        tvrg.column('idade', minwidth=0, width=60)
+        tvrg.column('email', minwidth=0, width=300)
+        
+        tvrg.heading('data', text='DATA')  
+        tvrg.heading('nome', text='NOME')
+        tvrg.heading('idade', text='IDADE')
+        tvrg.heading('email', text='EMAIL')
+            
+        tvrg.pack()
+            
+        for (a, b, c, d) in registros:
+            tvrg.insert("", 'end', values=(a, b, c, d))
+            
+        janela_rg.mainloop()
+    else:
+        messagebox.showwarning('ERROR', 'Não existem registros a serem exibidos.')     
+ 
+ 
+def cadastrar(entry_data, entry_nome, entry_idade, entry_email):
+    nome = entry_nome.get()
+    idade = entry_idade.get()
+    email = entry_email.get()
+    data = entry_data.get()
+
+    if data and nome and idade and email:
+        salvar_registro(data, nome, idade, email)
+        entry_data.delete(0,tk.END)
+        entry_nome.delete(0, tk.END)
+        entry_idade.delete(0, tk.END)
+        entry_email.delete(0, tk.END)
+    else:
+        messagebox.showwarning('Campos vazios', 'Por favor, preencha todos os campos.')
+        
+
+def pesquisa_nome(entry_pesquisa_nome):
+    with open('registros1.csv', mode='r') as file:
+        reader = csv.reader(file)
+        registros = list(reader)
+
+    nome_pesquisa = entry_pesquisa_nome.get()
+
+    registros_encontrados = [registro for registro in registros if nome_pesquisa in registro[1]]
+
+    def comando_personalizado():
+        janela_rg.destroy()
+        apagar_registro()
+        
+    
+    if registros_encontrados:
+        # Criando uma janela para exibir a lista de registros
+        janela_rg = tk.Toplevel()
+        janela_rg.title('Registros')
+        janela_rg.geometry("600x250")
+            
+        tvrg = ttk.Treeview(janela_rg, columns=('data','nome', 'idade', 'email',), show='headings')
+         
+        tvrg.column('data', minwidth=0, width=90)   
+        tvrg.column('nome', minwidth=0, width=200)
+        tvrg.column('idade', minwidth=0, width=60)
+        tvrg.column('email', minwidth=0, width=300)
+        
+        tvrg.heading('data', text='DATA')  
+        tvrg.heading('nome', text='NOME')
+        tvrg.heading('idade', text='IDADE')
+        tvrg.heading('email', text='EMAIL')
+            
+        tvrg.pack()
+            
+        for (a, b, c, d) in registros_encontrados:
+            tvrg.insert("", 'end', values=(a, b, c, d))
+            
+        janela_rg.mainloop()
+    else:
+        messagebox.showwarning('ERROR', 'Não existem registros a serem exibidos.')
+    
+    botao_apagar_2 = tk.Button(janela_rg, text='Apagar Registros', command=comando_personalizado)
+    botao_apagar_2.pack()
+    
+    
+def apagar_registro():
+    # Abrindo o arquivo CSV que contém as informações de registro
+    with open('registros1.csv', mode='r') as file:
+        reader = csv.reader(file)
+        registros = list(reader)
+
+    if registros:
+        # Criando uma janela para exibir a lista de registros
+        janela_delete = tk.Toplevel()
+        janela_delete.title('Excluir Registros')
+        janela_delete.geometry("600x250")
+        
+        # Criando o Treeview
+        tree = ttk.Treeview(janela_delete, columns=('data', 'nome', 'idade', 'email'), show='headings')
+        
+        tree.column('data', minwidth=0, width=90)
+        tree.column('nome', minwidth=0, width=200)
+        tree.column('idade', minwidth=0, width=60)
+        tree.column('email', minwidth=0, width=300)
+            
+        tree.heading('nome', text='NOME')
+        tree.heading('idade', text='IDADE')
+        tree.heading('email', text='EMAIL')
+        
+        for (a, b, c, d) in registros:
+            tree.insert("", 'end', values=(a, b, c, d))
+        
+        tree.pack()
+
+        def confirmar_exclusao():
+            # Obtendo o registro selecionado
+            item_selecionado = tree.selection()[0]
+            
+            # Obtendo o índice numérico do registro selecionado
+            indice_selecionado = int(tree.index(item_selecionado))
+            
+            # Obtendo os valores do registro selecionado
+            registro_selecionado = tree.item(item_selecionado)['values']
+            
+            # Removendo o registro da lista
+            registros.pop(indice_selecionado)
+
+            # Atualizando o Treeview
+            tree.delete(item_selecionado)
+
+            # Sobrescrevendo o arquivo CSV com os registros atualizados
+            with open('registros1.csv', mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(registros)
+
+            janela_delete.destroy()  # Fecha a janela de seleção de registros
+            messagebox.showinfo('Registro Removido', f'O registro {registro_selecionado} foi removido com sucesso!')
+            
+        botao_confirmar = tk.Button(janela_delete, text='Deletar registro selecionado', command=confirmar_exclusao)
+        botao_confirmar.pack()
+    else:
+        messagebox.showerror('ERROR', 'NÃO EXISTEM REGISTROS A SEREM APAGADOS!')
+    
